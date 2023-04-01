39853313963f832b46485d88a05a2fee127e782d
MrAalu <lulamit16@gmail.com>
2023-04-01 16:57:34 +0545

    URLConn HeaderContent Fetching Methods

--- Loginpage/addedimage.java
+++ Loginpage/addedimage.java
@@ -1,97 +1,68 @@
 package Loginpage;
 
-import javax.swing.*;
-
-
 import java.awt.event.*;
+import javax.swing.*;
 import javax.swing.ImageIcon;
 
+public class addedimage implements ActionListener {
 
-public class addedimage implements ActionListener{
+  JTextField tfpw;
+  JTextField tfuser; //// textfield declaration which is global variable.
+  JButton Login;
+  JFrame f;
 
-    JTextField tfpw;
-    JTextField tfuser;  //// textfield declaration which is global variable.
-    JButton Login;       
-    JFrame f;
+  public addedimage() {
+    f = new JFrame("Login");
 
-    
-    
+    JLabel luser = new JLabel("Username");
+    luser.setBounds(20, 20, 150, 25); // Username
+    f.add(luser);
 
-    public addedimage()
-    {
-        f= new JFrame("Login");
+    tfuser = new JTextField();
+    tfuser.setBounds(180, 20, 150, 25); // Textfield of username
+    f.add(tfuser);
 
-        JLabel luser = new JLabel("Username");
-        luser.setBounds(20,20,150,25); // Username
-        f.add(luser);
+    JLabel lpw = new JLabel("Password");
+    lpw.setBounds(20, 55, 150, 25); // Password
+    f.add(lpw);
 
-        tfuser = new JTextField();
-        tfuser.setBounds(180,20,150,25); // Textfield of username
-        f.add(tfuser);
+    tfpw = new JTextField();
+    tfpw.setBounds(180, 55, 150, 25); // Textfield of Password
+    f.add(tfpw);
 
-        JLabel lpw = new JLabel("Password");
-        lpw.setBounds(20,55,150,25); // Password
-        f.add(lpw);
+    Login = new JButton("Login");
 
-        tfpw = new JTextField();
-        tfpw.setBounds(180,55,150,25); // Textfield of Password
-        f.add(tfpw);
+    Login.addActionListener(this);
+    Login.setBounds(180, 90, 150, 25); // For button
+    f.add(Login);
 
+    f.setSize(500, 500);
+    f.setLayout(null); ///// For frame
+    f.setVisible(true);
+  }
 
-        Login = new JButton("Login");
-       
-        Login.addActionListener(this);
-        Login.setBounds(180,90,150,25); // For button
-        f.add(Login);
+  public static void main(String[] args) {
+    new addedimage();
+  }
 
-        
+  @Override
+  public void actionPerformed(ActionEvent e) { //factoryMethod(actionPerdormed)
+    String luser = tfuser.getText();
+    String lpw = tfpw.getText();
 
+    if (luser.equals("admin") && lpw.equals("admin")) {
+      ImageIcon icon = new ImageIcon("ASHISH.png");
+      JOptionPane.showMessageDialog(
+        null,
+        "WELCOME SUCCESSFUL PERSON",
+        "wow",
+        JOptionPane.INFORMATION_MESSAGE,
+        icon
+      );
 
-        f.setSize(500,500);
-        f.setLayout(null);    ///// For frame
-        f.setVisible(true);
-        
-
-       
-        
+      System.out.println("Loged In");
+    } else {
+      System.out.println("Error");
     }
-    public static void main(String[] args) {
-        new addedimage();
-        }
- 
-
-        @Override
-        public void actionPerformed(ActionEvent e) {  //factoryMethod(actionPerdormed)
-            
-            String luser = tfuser.getText();
-            String lpw = tfpw.getText();
-        
-            if(luser.equals("admin")&& lpw.equals("admin")){
-           
-                
-
-                ImageIcon icon = new ImageIcon("ASHISH.png");
-                JOptionPane.showMessageDialog(null,"WELCOME SUCCESSFUL PERSON","wow",JOptionPane.INFORMATION_MESSAGE,icon );
-                
-                
-            
-                
-                System.out.println("Loged In");
-    
-            }
-            else{
-                System.out.println("Error");
-            }
-        }
-
-
-
-    }
-
-
-
-
-    
-   
-  
-
+  }
+}
---
+++ urlHeaderFieldMethods/URLHeaderMethods.java
@@ -0,0 +1,88 @@
+package urlHeaderFieldMethods;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.Arrays;
+import java.util.Date;
+
+class URLHeaderMethods {
+
+  public static void main(String[] args)
+    throws MalformedURLException, IOException {
+    URL url = new URL("https://www.google.com");
+    URLConnection uc = url.openConnection();
+
+    // 1.returns MIME Type -> Mime Identifies content data types i.e. text/html
+    System.out.println("1.Content Type : " + uc.getContentType());
+
+    // 2.returns content lenghth, '-1' if content is unknow or cannot be determined
+    System.out.println("2.Content Length : " + uc.getContentLength());
+
+    // 3.Retrieves date in long value
+    System.out.println("3.getDate() : " + uc.getDate());
+
+    // Storing getDate() to variable to convert it to date
+    long date_time = uc.getDate();
+
+    // Using Date class to convert long into date.
+    Date date = new Date(date_time);
+    System.out.println("Date/Time : " + date);
+
+    // 4.content last modified date
+    System.out.println("4.getLastModified : " + uc.getLastModified());
+
+    // 5.returns content's ma set gareko expiry date
+    System.out.println("5.getExpiration : " + uc.getExpiration());
+
+    // 6.returns kun encoding use vaxa for retrieving content from url
+    System.out.println("6.getContentEncoding : " + uc.getContentEncoding());
+
+    // <------------------------------------------------------------------------------------------->
+
+    // 7.returns contents of resources in , text -> string , binary ->byte[]
+    // Note : String vaye not ARRAY,single line single variable mai sab hunxa , BYTE vaye Array FORM
+    System.out.println("7.getContent() as Obj : " + uc.getContent());
+
+    // getContent through fetch gareko unknown content type lai Object variable ma store gareko
+    Object get_content_obj = uc.getContent();
+
+    // We dont know getContent bata Text/Binary data aayo ? so, algorithm ko through convert gareko
+
+    // If fetched content is text/html (String) then converting to String and Printing
+    if (get_content_obj instanceof String) {
+      String get_content_isString = (String) get_content_obj;
+      System.out.println("Content as a String: " + get_content_isString);
+      // If fetched content is binary (byte) then converting to byte and Printing
+    } else if (get_content_obj instanceof byte[]) {
+      byte[] get_content_isByte = (byte[]) get_content_obj;
+      System.out.println(
+        "Content as a byte array: " + Arrays.toString(get_content_isByte)
+      );
+    } else {
+      System.out.println("Content type not recognized");
+    }
+
+    // <------------------------------------------------------------------------------------------->
+
+    // 8. "Content-Type" kun ho headerfield ko tyo Fetch garxa using getHeaderField
+    String contentType = uc.getHeaderField("Content-Type");
+    System.out.println("8.Content-Type: " + contentType);
+
+    //  9.InputStream le full webcontent fetch gareko
+    InputStream input_stream = uc.getInputStream();
+    int input;
+    // variable form mai print gareko just to be clear,below is the code that prints the contents fully
+    System.out.print("9." + input_stream);
+    while ((input = input_stream.read()) != -1) {
+      // System.out.print((char)input);
+    }
+    // 10.output_stream used to write data to the output stream, which will be sent to the server.
+    // uc.setDoOutput(false);
+    // OutputStream output_stream = uc.getOutputStream();
+
+  }
+}
