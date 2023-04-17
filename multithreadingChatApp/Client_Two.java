4261525333a89f0ba3a8efd319cbfbbd6fad0b72
MrAalu <lulamit16@gmail.com>
2023-04-17 20:10:31 +0545

    Multithreading Socket Programming (Handling Multiple Clients)

---
+++ multithreadingChatApp/Client_Two.java
@@ -0,0 +1,36 @@
+package multithreadingChatApp;
+
+import java.net.*;
+import java.io.*;
+import java.util.Scanner;
+
+public class Client_Two {
+ public static void main(String[] args) {
+
+  Scanner scanner = new Scanner(System.in);
+
+  try {
+
+   System.out.println("Client TWO Started !");
+   Socket client_socket = new Socket("localhost", 8888);
+   System.out.println("Client Connection to Server Established " + client_socket.getRemoteSocketAddress());
+
+   while (true) {
+
+    // Sending data to Client
+    DataOutputStream data_output_stream = new DataOutputStream(client_socket.getOutputStream());
+    System.out.print("Enter Message : ");
+    String output = scanner.nextLine();
+    data_output_stream.writeUTF(output);
+
+    // Reading Data from Server
+    DataInputStream data_input_stream = new DataInputStream(client_socket.getInputStream());
+    String input = data_input_stream.readUTF();
+    System.out.println("Server says : " + input);
+
+   }
+  } catch (Exception e) {
+   System.out.println("Error in Client : " + e.getMessage());
+  }
+ }
+}
---
+++ multithreadingChatApp/ServerSide.java
@@ -0,0 +1,68 @@
+package multithreadingChatApp;
+
+import java.net.*;
+import java.io.*;
+import java.util.Scanner;
+
+public class ServerSide {
+ public static void main(String[] args) {
+  // TODO this program doesnt function properly, yet to be fixed
+  // issue : one new client joins, server no longer can chat with previous client
+
+  try {
+
+   ServerSocket server_socket = new ServerSocket(8888);
+   System.out.println("Server Started !");
+
+   System.out.println("Waiting for Client");
+
+   while (true) {
+
+    Socket client_socket = server_socket.accept();
+    System.out.println("Client Connected : " + client_socket.getLocalSocketAddress());
+
+    // Thread class ko Object create gareko and tesko vitra constructor call garera
+    // Args pass gareko
+    Thread thread_object = new Thread(new ServerMultithreadHandler(client_socket));
+
+    thread_object.start();
+
+   }
+  } catch (Exception e) {
+   System.out.println("Error in Server : " + e.getMessage());
+  }
+ }
+}
+
+// Class created to handle multiple clients connection using multithreading
+// technology
+
+class ServerMultithreadHandler implements Runnable {
+
+ Scanner scanner = new Scanner(System.in);
+ private Socket client_socket;
+
+ ServerMultithreadHandler(Socket socket_parameter) {
+  this.client_socket = socket_parameter;
+ }
+
+ public void run() {
+
+  try {
+
+   // Reading Client Data
+   DataInputStream data_input_stream = new DataInputStream(client_socket.getInputStream());
+   String client_input = data_input_stream.readUTF();
+   System.out.println("Client Says : " + client_input);
+
+   // Sending Feedback to Client
+   DataOutputStream data_output_stream = new DataOutputStream(client_socket.getOutputStream());
+   System.out.print("Enter Message : ");
+   String output = scanner.nextLine();
+   data_output_stream.writeUTF(output);
+
+  } catch (Exception e) {
+   System.out.println("Error in Server Thread Handling : " + e.getMessage());
+  }
+ }
+}
