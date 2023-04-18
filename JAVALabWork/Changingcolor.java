b336d8afcb78a1c228aa546806974adc5fc3e318
Ashish <meashish2020@gmail.com>
2023-04-18 18:36:29 +0545

    Java-ALL(LabWorks)

---
+++ JAVALabWork/Changingcolor.java
@@ -0,0 +1,47 @@
+package JAVALabWork;
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+// import javax.swing.JPanel;
+import javax.swing.JPanel;
+
+
+public class Changingcolor extends JFrame implements ActionListener {
+  
+  JButton colorButton;
+  
+  public Changingcolor() {
+    super("Color Button UI");
+    setSize(300, 200);
+    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    
+    JPanel panel = new JPanel();
+    colorButton = new JButton("Click to Change Color");
+    colorButton.addActionListener(this);
+    panel.add(colorButton);
+    
+    add(panel);
+    setVisible(true);
+  }
+  
+  @Override
+  public void actionPerformed(ActionEvent e) {
+
+    if (e.getSource() == colorButton) {   // which button is clicked.
+      Color currentColor = colorButton.getBackground();
+      if (currentColor == Color.GRAY) {
+        colorButton.setBackground(Color.GREEN);
+      } else {
+        colorButton.setBackground(Color.gray);
+      }
+    }
+  }
+  
+
+
+  public static void main(String[] args) {
+    new Changingcolor();
+  }
+}
---
+++ JAVALabWork/TExtSizecolorwithLayoutmanager.java
@@ -0,0 +1,32 @@
+/*Java Program to Display Text with Background Color as Cyan*/
+package JAVALabWork;
+
+import javax.swing.*;
+import java.awt.*;
+
+ public class TExtSizecolorwithLayoutmanager
+{
+    //Driver function
+    public static void main(String args[])
+    {
+        //Create a frame
+	JFrame frame = new JFrame("LayoutManager");
+
+	frame.setSize(400,400);
+	frame.setLayout(new FlowLayout());
+	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  
+	frame.getContentPane().setBackground(Color.CYAN);
+	//Write some text to the frame
+
+
+	String str="The Background of Frame is Cyan";
+	JLabel text = new JLabel(str);
+	text.setFont(new Font("Serif",Font.ITALIC,20));
+	frame.add(text);
+
+	//Set Background Color of Text as red
+	text.setForeground(Color.BLUE);
+	//Display the frame
+	frame.setVisible(true);
+    }
+}
