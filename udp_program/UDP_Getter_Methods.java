ded7c8995caec343d5d92ea2adef5fbc2a51c76d
MrAalu <lulamit16@gmail.com>
2023-05-09 14:11:27 +0545

    UDP Datagram Getter/Setter Methods

---
+++ udp_program/UDP_Getter_Methods.java
@@ -0,0 +1,40 @@
+package udp_program;
+
+import java.net.*;
+
+public class UDP_Getter_Methods {
+ public static void main(String[] args) {
+
+  try {
+
+   int port = 5000;
+
+   byte byte_data[] = new byte[1024];
+
+   DatagramSocket datagram_socket = new DatagramSocket(port);
+
+   DatagramPacket datagram_packet = new DatagramPacket(byte_data, byte_data.length);
+
+   datagram_socket.receive(datagram_packet);
+
+   // GETTER METHODS
+   System.out.println("Remote Host IP : " + datagram_packet.getAddress());
+
+   System.out.println(datagram_packet.getPort());
+
+   System.out.println(datagram_packet.getSocketAddress());
+
+   System.out.println(new String(datagram_packet.getData()));
+
+   System.out.println(datagram_packet.getLength());
+
+   System.out.println(datagram_packet.getOffset());
+
+   // Closing the Socket
+   datagram_socket.close();
+
+  } catch (Exception e) {
+   System.out.println("ERROR : " + e.getMessage());
+  }
+ }
+}
---
+++ udp_program/UDP_Setter_Methods.java
@@ -0,0 +1,41 @@
+package udp_program;
+
+import java.net.*;
+
+public class UDP_Setter_Methods {
+ public static void main(String[] args) {
+
+  try {
+
+   byte[] data = "hello bro".getBytes();
+
+   int port = 5000;
+
+   InetAddress inet_address = InetAddress.getByName("localhost");
+
+   DatagramSocket datagram_socket = new DatagramSocket();
+
+   DatagramPacket datagram_packet = new DatagramPacket(data, data.length);
+
+   // SETTER METHODS - 4
+
+   // Modify DATA
+   datagram_packet.setData("action".getBytes());
+
+   // sends 'n' characters
+   datagram_packet.setLength(3);
+
+   datagram_packet.setAddress(inet_address);
+
+   datagram_packet.setPort(port);
+
+   // Sending the Data and Closing the Socket
+   datagram_socket.send(datagram_packet);
+   datagram_socket.close();
+
+  } catch (Exception e) {
+   System.out.println("ERROR : " + e.getMessage());
+  }
+
+ }
+}
