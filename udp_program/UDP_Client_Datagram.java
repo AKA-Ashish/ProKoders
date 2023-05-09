b04075b29248901684e13146520f708c158f811d
MrAalu <lulamit16@gmail.com>
2023-05-09 13:19:37 +0545

    UDP Datagram Program

---
+++ udp_program/UDP_Client_Datagram.java
@@ -0,0 +1,37 @@
+package udp_program;
+
+import java.io.IOException;
+import java.net.*;
+
+class UDP_Client_Datagram {
+
+ public static void main(String[] args) {
+
+  try {
+
+   byte[] data = "hello bro".getBytes();
+
+   int port = 5000;
+
+   InetAddress inet_address = InetAddress.getByName("localhost");
+
+   DatagramSocket datagram_socket = new DatagramSocket();
+
+   DatagramPacket datagram_packet = new DatagramPacket(data, data.length, inet_address, port);
+
+   datagram_socket.send(datagram_packet);
+
+   datagram_socket.close();
+
+  } catch (SocketException e) {
+   System.out.println("ERROR : " + e.getMessage());
+  } catch (UnknownHostException e) {
+   System.out.println("ERROR : " + e.getMessage());
+  } catch (IOException e) {
+   System.out.println("ERROR : " + e.getMessage());
+
+  }
+
+ }
+
+}
---
+++ udp_program/UDP_Server_Datagram.java
@@ -0,0 +1,34 @@
+package udp_program;
+
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+
+public class UDP_Server_Datagram {
+ public static void main(String[] args) {
+
+  try {
+
+   int port = 5000;
+
+   byte byte_data[] = new byte[1024];
+
+   DatagramSocket datagram_socket = new DatagramSocket(port);
+
+   DatagramPacket datagram_packet = new DatagramPacket(byte_data, byte_data.length);
+
+   datagram_socket.receive(datagram_packet);
+
+   String message = new String(datagram_packet.getData(), datagram_packet.getOffset(),
+     datagram_packet.getLength());
+
+   System.out.println("Received message: " + message);
+
+   datagram_socket.close();
+
+  } catch (Exception e) {
+   System.out.println("ERROR : " + e.getMessage());
+  }
+
+ }
+
+}
