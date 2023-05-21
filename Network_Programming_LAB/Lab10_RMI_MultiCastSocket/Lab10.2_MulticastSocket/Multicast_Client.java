14234381c9e3df3c10230913b6e0dceeb0a79a65
MrAalu <lulamit16@gmail.com>
2023-05-21 14:51:25 +0545

    Lab10 MulticastSocket Data sending and Receiving

---
+++ Network_Programming_LAB/Lab10_RMI_MultiCastSocket/Lab10.2_MulticastSocket/Multicast_Client.java
@@ -0,0 +1,36 @@
+import java.net.DatagramPacket;
+import java.net.InetAddress;
+import java.net.MulticastSocket;
+
+// We can create multiple clients and all of them will receive the data sended by Multicast Server
+@SuppressWarnings("resource")
+public class Multicast_Client {
+ public static void main(String[] args) {
+
+  try {
+
+   int port = 5000;
+
+   byte buffer[] = new byte[1024];
+
+   InetAddress inetAddress = InetAddress.getByName("224.0.0.1");
+
+   MulticastSocket multicast_socket = new MulticastSocket(port);
+
+   multicast_socket.joinGroup(inetAddress);
+
+   while (true) {
+    DatagramPacket datagram_packet = new DatagramPacket(buffer, buffer.length);
+
+    multicast_socket.receive(datagram_packet);
+
+    String data = new String(buffer, 0, buffer.length);
+
+    System.out.println("Received data : " + data);
+   }
+
+  } catch (Exception e) {
+   System.out.println("Error in Client : " + e.getMessage());
+  }
+ }
+}
